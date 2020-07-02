0acc253c33fd72157da8e30162965a892d64c1ba
GGFERCAS13 <gonzalo.gut.mes@techtalents.club>
2020-07-02 13:14:47 +0200

    testing how long does it takes to get to a 100 speed.

--- data_modeling.py
+++ data_modeling.py
@@ -1,8 +1,41 @@
+import os
+from termcolor import colored
+import time
 
-class Direction:
-    north = 1
-    east = 2
-    south = 3
-    west = 4
+class Car:
 
-# print(Direction.east)
+    def __init__(self, custom_speed):
+        self.position = 0
+        self.speed = custom_speed
+
+    def drive(self):
+        self.position += self.speed
+    
+    def print_info(self):
+        print("Car is current at:", self.position, "with an speed of:", self.speed, end='\r')
+
+
+
+
+# nicos_car = Car()
+# diegos_car = Car()
+gonzalos_car = Car(0)
+
+start_time = time.time()
+for i in range(1, 200):
+    time.time()
+    gonzalos_car.speed = i
+    gonzalos_car.drive()
+    gonzalos_car.print_info()
+    time.sleep(1)
+    if gonzalos_car.speed >= 100:
+        end_time = time.time()
+        final_time = end_time - start_time
+        print('\n', final_time)
+        break
+print()
+
+# gonzalos_car.print_info()
+# gonzalos_car.drive()
+# gonzalos_car.drive()
+# gonzalos_car.print_info()
