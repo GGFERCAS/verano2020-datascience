fb6aa7adb230b28bc54dc057b382cf3b0e509175
GGFERCAS13 <gonzalo.gut.mes@techtalents.club>
2020-07-03 13:26:25 +0200

    final correction.

--- self_inspection.py
+++ self_inspection.py
@@ -3,14 +3,14 @@ class Car:
     def create_new(name, speed):
         car_info = {
             "name": name,
-            "position": position,
+            "position": 0,
             "speed": speed,
         }
         return car_info
     
     def move(car_info):
         car_info["position"] += car_info["speed"]
-        print(car_info["name"] + " is a now at position " + str(car_info["position"] + " he is going at " + str(car_info[speed]) + "KM."))
+        print(car_info["name"] + " is a now at position " + str(car_info["position"]))
 
 gonzalos_car = Car
 gonzalos_info = gonzalos_car.create_new("Gonzalo", 100)
