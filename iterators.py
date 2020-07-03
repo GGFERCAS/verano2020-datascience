220a2a6759bfd2372d3c0b73637e40df947bfc33
GGFERCAS13 <gonzalo.gut.mes@techtalents.club>
2020-07-03 13:59:02 +0200

    make the operation works.

--- iterators.py
+++ iterators.py
@@ -1,5 +1,20 @@
 
 
 class Powers:
-    def __init__(self, indice):
-        self.indice = indice
+    def __init__(self, exponent):
+        self.exponent = exponent
+
+    def __iter__(self):
+        self.base = 0
+        return self
+
+    def __next__(self):
+        self.base += 1
+        return self.base ** self.exponent
+
+
+squares = Powers(2)
+
+
+for n in squares:
+    print(n)
