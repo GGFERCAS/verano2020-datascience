3f49e31d6bb8670728dede9b9d28a37406d37112
GGFERCAS13 <gonzalo.gut.mes@techtalents.club>
2020-07-03 12:14:22 +0200

    made changes

--- iterators.py
+++ iterators.py
@@ -1,5 +1,11 @@
-print("Python is awesome!")
+for char in "Python is awesome!":
+    print(char)
 
-print([3, 1, 4, 1, 2, 4, 2, 3, 2, 1, 4 ,5])
+for n in [3, 1, 4, 1, 2, 4, 2, 3, 2, 1, 4 ,5]:
+    print(n)
 
-print({"first-name": "Bob", "last-name": "the Builder", "age": 42})
+for x in {"first-name": "Bob", "last-name": "the Builder", "age": 42}:
+    print(x)
+
+for line in open("data_modelling.py", "r"):
+    print("->", line)
