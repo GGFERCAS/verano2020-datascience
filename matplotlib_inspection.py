94ab14211b3181590c1113a9edd44f50ad4fba18
GGFERCAS13 <gonzalo.gut.mes@techtalents.club>
2020-07-07 12:44:25 +0200

    made an xticks

--- matplotlib_inspection.py
+++ matplotlib_inspection.py
@@ -1,16 +1,20 @@
 
 import math
+import numpy as np
 
 from matplotlib import pyplot
 
 
-x_coords = range(1, 11)
+x_coords = np.arange(1, 11, 0.001)
 y_coords = [math.sin(x) for x in x_coords]
 pyplot.plot(x_coords, y_coords)
 # pyplot.plot(x_coords, y_coords)
 
 # pyplot.xlim(0, 10)
 # pyplot.ylim(-5, 10)
+
+# pyplot.xticks([1,2,3,4,5,6,7,8,9,10,11])
+
 pyplot.show()
 
 # pyplot.savefig("output.png")
