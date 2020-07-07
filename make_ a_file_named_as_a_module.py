085bf2548cde2198f5d85d10c298655100b47a15
GGFERCAS13 <gonzalo.gut.mes@techtalents.club>
2020-07-07 12:25:50 +0200

    changed an error in pop function

--- make_ a_file_named_as_a_module.py
+++ make_ a_file_named_as_a_module.py
@@ -2,6 +2,6 @@
 # You could now call this file matplotlib without the code crushing.
 
 import sys
-sys.path.append(sys.path.popç(0))
+sys.path.append(sys.path.pop(0))
 
 from matplotlib import pyplot
