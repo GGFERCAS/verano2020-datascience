5290b6a8160b30c885f2565824d0645183da684a
GGFERCAS13 <gonzalo.gut.mes@techtalents.club>
2020-07-08 13:32:34 +0200

    made it make a file called log that store every IP that send you things

--- curiosities/host_data_transmition.py
+++ curiosities/host_data_transmition.py
@@ -4,6 +4,15 @@ app = flask.Flask(__name__)
 @app.route("/data")
 def data():
     name = flask.request.args.get("name")
+    file.write(flask.request.remote_addr + "\n")
     print(name)
     return ""
+
+@app.route("/close")
+def close_server():
+    file.close()
+    exit()
+file = open("log", "w")
+
 app.run(host="0.0.0.0", port=3001)
+file.close()
