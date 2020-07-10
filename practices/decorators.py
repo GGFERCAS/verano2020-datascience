8a66044c52e858f7c12c3ba6458b0146f85a1c7d
GGFERCAS13 <gonzalo.gut.mes@techtalents.club>
2020-07-10 13:56:13 +0200

    worked with decorators

--- practices/decorators.py
+++ practices/decorators.py
@@ -2,13 +2,18 @@
 import requests
 import timeit
 
+def timer(function):
+    def wrapper():
+        start_timing = timeit.default_timer()  
+        function()
+        end_time = timeit.default_timer()
+        print("It took {} seconds to run".format(end_time - start_timing))
+    return wrapper
+
+
 def do_something():
     url = "https://www.google.co.uk"
     requests.get(url)
 
-
-start_timing = timeit.default_timer()
-do_something()
-end_time = timeit.default_timer()
-
-print("It took {} seconds to run".format(end_time - start_timing))
+x = timer(do_something)
+x()
