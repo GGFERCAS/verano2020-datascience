97fbc7518d580112f9dcf97cbba35d39a99b058b
GGFERCAS13 <gonzalo.gut.mes@techtalents.club>
2020-07-10 13:04:10 +0200

    made a dicctionary

--- police-shooting/police_shooting.py
+++ police-shooting/police_shooting.py
@@ -7,53 +7,20 @@ contents = csv.reader(file)
 
 processors = [int, str, str, str, str, int, str, str, str, str, str, str, str, str]
 
-
-def process_policemans(contents: csv.reader):
-    column_names = next(contents)
-    for row in contents:
-        processed_data = []
-        for proc, data in zip(processors, row):
-            try:
-                processed_data.append(proc(data))
-            except ValueError:
-                break
-        else:
-            police = {name: data for name, data in zip(column_names, processed_data)}
-            yield police
-
-
-policeman = process_policemans(contents)
-
-def get_age(police):
-    return police["age"]
-
-def get_manner_of_death(police):
-    return police["manner_of_death"]
-
-shoted = 'shot'
-def get_all_shoted(police):
-    if police["manner_of_death"] == shoted:
-        print(police)
-
-
-if __name__ == '__main__':
-    print()
-
-    # policeman_searching = input("Name of the policeman: ")
-    # for police in policeman:
-    #     if police["name"] == policeman_searching:
-    #         print(police)
-
-    # oldest_guy = max(policeman, key=get_age)
-    # print(oldest_guy)
-
-    # shoted = 'shot'
-    # for police in policeman:
-    #     if police["manner_of_death"] == shoted:
-    #         print(police)
-
-    # def get_middle_age(police):
-    #     all_ages = 
-
-
-    print()
+for row in contents:
+    meteor = {
+        "id": row[0],
+        "name": row[1],
+        "date": row[2],
+        "manner_of_death": row[3],
+        "armed": row[4],
+        "age": row[5],
+        "gender": row[6],
+        "race": row[7],
+        "city": row[8],
+        "state": row[9],  
+        "signs_of_mental": row[10],    
+        "threat_level": row[11],  
+        "flee": row[12], 
+        "body_camera": row[13],  
+    }
